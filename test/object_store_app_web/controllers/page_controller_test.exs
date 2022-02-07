247cb4cffb1d0a262c0e49157fc9269e52005324
Thomas <thomastimmer11@hotmail.com>
2022-02-07 20:37:33 +0100

    test: fix get / test

--- test/object_store_app_web/controllers/page_controller_test.exs
+++ test/object_store_app_web/controllers/page_controller_test.exs
@@ -1,8 +1,8 @@
 defmodule ObjectStoreAppWeb.PageControllerTest do
   use ObjectStoreAppWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
+  test "GET / redirect to login", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 302) =~ "redirected"
   end
 end
